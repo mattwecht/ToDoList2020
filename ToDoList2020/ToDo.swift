3876c547c69354c7e30e0d76ad8fa8262ade46d8
Matt Wecht <mattwecht@gmail.com>
2020-05-02 21:09:45 -0400

    Complete Iteration 0

---
+++ ToDoList2020/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoList2020
+//
+//  Created by Matthew Wecht on 5/2/20.
+//  Copyright © 2020 Matthew Wecht. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+    
+}
+
